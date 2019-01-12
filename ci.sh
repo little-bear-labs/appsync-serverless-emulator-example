6b103ca6d184fa763350167c676bb8dd57f30b01
cvci <james@conduit.vc>
2019-01-12 15:54:54 -0700

    turn on more logging

--- ci.sh
+++ ci.sh
@@ -1,11 +1,11 @@
 #! /bin/bash -ex
 
 echo "Testing with yarn install"
-yarn test
+APPSYNC_EMULATOR_LOG=1 yarn test
 
 # We have previously run into issues where we have a working build on yarn
 # but not with npm.
 echo "Testing with npm install"
 rm -rf node_modules
-npm i 
+APPSYNC_EMULATOR_LOG=1 npm i 
 DEBUG=1 npm test
