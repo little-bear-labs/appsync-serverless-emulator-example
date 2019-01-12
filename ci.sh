b07abe100a064d4008f612c1bc07d1b8833271fa
cvci <james@conduit.vc>
2019-01-12 13:28:22 -0700

    Test on node lts

--- ci.sh
+++ ci.sh
@@ -3,6 +3,8 @@
 echo "Testing with yarn install"
 yarn test
 
+# We have previously run into issues where we have a working build on yarn
+# but not with npm.
 echo "Testing with npm install"
 rm -rf node_modules
 npm i
