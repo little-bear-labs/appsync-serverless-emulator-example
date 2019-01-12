2756fed384fc012b632f3a687ac2142011e8945b
cvci <james@conduit.vc>
2019-01-12 15:03:23 -0700

    Dump version numbers

--- ci.sh
+++ ci.sh
@@ -1,6 +1,7 @@
 #! /bin/bash -ex
 
 echo "Testing with yarn install"
+yarn list
 yarn test
 
 # We have previously run into issues where we have a working build on yarn
@@ -8,4 +9,5 @@ yarn test
 echo "Testing with npm install"
 rm -rf node_modules
 npm i
+npm ls --depth 2
 yarn test
