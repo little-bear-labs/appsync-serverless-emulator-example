dbc244daa38de8adf2604a910ee2df31d1f06323
cvci <james@conduit.vc>
2019-01-12 15:07:37 -0700

    Remove very verbose listing of npm modules

--- ci.sh
+++ ci.sh
@@ -1,13 +1,11 @@
 #! /bin/bash -ex
 
 echo "Testing with yarn install"
-yarn list
 yarn test
 
 # We have previously run into issues where we have a working build on yarn
 # but not with npm.
 echo "Testing with npm install"
 rm -rf node_modules
-npm i
 npm ls --depth 2
 yarn test
